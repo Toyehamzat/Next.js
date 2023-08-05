48189cdc9e7a7506e9b83c8b0ba7a09ec370ed76
Toyehamzat <anibiseun@gmail.com>
2023-08-05 14:01:36 +0100

    routing

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,11 @@
-export default function page() {
-  return <></>;
+import Link from "next/link";
+export default function About() {
+  return (
+    <>
+      <h1 className="flex flex-col items-center justify-between p-24">
+        About
+      </h1>
+      <Link href="/">Go home</Link>
+    </>
+  );
 }
